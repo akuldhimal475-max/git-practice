0b6ba4cd4f4111bf371ec781b0e3a031e7945faf
Akul <akuldhimal475@gmail.com>
2026-07-17 21:15:57 +0545

    Updated basic Calculation tab

--- fold-react/web.js
+++ fold-react/web.js
@@ -1,3 +1,28 @@
-function run() {
-  document.querySelector("#head").textContent = "My first Git work. Hats off";
-}
+document.querySelector(".btn_add").addEventListener("click", () => {
+  let a = document.querySelector(".num1").value;
+  let b = document.querySelector(".num2").value;
+  a = Number(a);
+  b = Number(b);
+  document.write(`The addition is ${a + b}`);
+});
+document.querySelector(".btn_sub").addEventListener("click", () => {
+  let a = document.querySelector(".num1").value;
+  let b = document.querySelector(".num2").value;
+  a = Number(a);
+  b = Number(b);
+  document.write(`The subtraction is ${a - b}`);
+});
+document.querySelector(".btn_mul").addEventListener("click", () => {
+  let a = document.querySelector(".num1").value;
+  let b = document.querySelector(".num2").value;
+  a = Number(a);
+  b = Number(b);
+  document.write(`The multiplicaion is ${a * b}`);
+});
+document.querySelector(".btn_div").addEventListener("click", () => {
+  let a = document.querySelector(".num1").value;
+  let b = document.querySelector(".num2").value;
+  a = Number(a);
+  b = Number(b);
+  document.write(`The diviion is ${a / b}`);
+});
